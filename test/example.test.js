c70069853bb3d0c96e1bcc19185d9421607cbbbe
David Delgadillo <daviddel.731@gmail.com>
2021-03-25 17:51:58 -0700

    Tested more edge cases

--- test/example.test.js
+++ test/example.test.js
@@ -7,13 +7,31 @@ const test = QUnit.test;
 test('if there is a draw, return draw', (expect) => {
     //Arrange
     // Set up your arguments and expectations
-    const expected = 'draw';
+    let expected = 'draw';
     
     //Act 
     // Call the function you're testing and set the result to a const
-    const actual = didUserWin('paper', 'paper');
+    let actual = didUserWin('paper', 'paper');
 
     //Expect
     // Make assertions about what is expected versus the actual result
+    expect.equal(actual, expected);
+});
+
+test('if there is a win, return win', (expect) => {
+
+    let expected = 'win';
+
+    let actual = didUserWin('paper', 'rock');
+
+    expect.equal(actual, expected);
+});
+
+test('if there is a loss, return lose', (expect) => {
+
+    let expected = 'lose';
+
+    let actual = didUserWin('paper', 'scissors');
+
     expect.equal(actual, expected);
 });
